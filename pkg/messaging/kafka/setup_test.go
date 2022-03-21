b227d0bd40aea15d2c3aeae8a14fa1967fb9fa7f
0x6f736f646f <blackd0t@protonmail.com>
2022-03-21 16:34:47 +0300

    Initial commit
    Signed-off-by: 0x6f736f646f <blackd0t@protonmail.com>

---
+++ pkg/messaging/kafka/setup_test.go
@@ -0,0 +1,74 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+package kafka_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
+
+	"github.com/mainflux/mainflux/logger"
+	"github.com/mainflux/mainflux/pkg/messaging"
+	"github.com/mainflux/mainflux/pkg/messaging/kafka"
+	dockertest "github.com/ory/dockertest/v3"
+)
+
+var (
+	publisher messaging.Publisher
+	pubsub    messaging.PubSub
+)
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	container, err := pool.Run("spotify/kafka", "latest", []string{"ADVERTISED_HOST=127.0.0.1", "ADVERTISED_PORT=9092"})
+	if err != nil {
+		log.Fatalf("Could not start container: %s", err)
+	}
+	handleInterrupt(pool, container)
+
+	address := fmt.Sprintf("%s:%s", "localhost", container.GetPort("9092/tcp"))
+	if err := pool.Retry(func() error {
+		publisher, err = kafka.NewPublisher(address)
+		return err
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	logger, err := logger.New(os.Stdout, "error")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	if err := pool.Retry(func() error {
+		pubsub, err = kafka.NewPubSub(address, "", logger)
+		return err
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	code := m.Run()
+	if err := pool.Purge(container); err != nil {
+		log.Fatalf("Could not purge container: %s", err)
+	}
+
+	os.Exit(code)
+}
+
+func handleInterrupt(pool *dockertest.Pool, container *dockertest.Resource) {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		if err := pool.Purge(container); err != nil {
+			log.Fatalf("Could not purge container: %s", err)
+		}
+		os.Exit(0)
+	}()
+}
