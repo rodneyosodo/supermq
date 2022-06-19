2b9a4833ada0052279a062eb7c94b4146a1c0a19
0x6f736f646f <blackd0t@protonmail.com>
2022-06-19 23:52:28 +0300

    add compile check
    Signed-off-by: 0x6f736f646f <blackd0t@protonmail.com>

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -81,6 +81,10 @@ setup() {
     setup_lint
 }
 
+compile_check(){
+    MF_BROKER_TYPE=nats make http
+}
+
 run_test() {
     echo "Running lint..."
     golangci-lint run --no-config --disable-all --enable gosimple --enable govet --enable unused --enable deadcode --timeout 3m
@@ -94,6 +98,8 @@ run_test() {
             rm profile.out
         fi
     done
+    echo "Compile check..."
+    compile_check
 }
 
 push() {
