848f9212a9d2db1f1c3b3061745e080d07c2ccc9
rodneyosodo <blackd0t@protonmail.com>
2023-04-18 15:32:42 +0300

    Optimize ID Size to be Max at 36
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- things/postgres/init.go
+++ things/postgres/init.go
@@ -10,37 +10,39 @@ func Migration() *migrate.MemoryMigrationSource {
 		Migrations: []*migrate.Migration{
 			{
 				Id: "clients_01",
+				// VARCHAR(36) for colums with IDs as UUIDS have a maximum of 36 characters
+				// STATUS 0 to imply enabled and 1 to imply disabled
 				Up: []string{
 					`CREATE TABLE IF NOT EXISTS clients (
-						id			VARCHAR(254) PRIMARY KEY,
+						id			VARCHAR(36) PRIMARY KEY,
 						name		VARCHAR(1024),
-						owner_id	VARCHAR(254),
+						owner_id	VARCHAR(36),
 						identity	VARCHAR(254),
-						secret		VARCHAR(4096) UNIQUE NOT NULL,
+						secret		VARCHAR(4096) NOT NULL UNIQUE,
 						tags		TEXT[],
 						metadata	JSONB,
 						created_at	TIMESTAMP,
 						updated_at	TIMESTAMP,
-						status		SMALLINT NOT NULL CHECK (status >= 0) DEFAULT 0,
+						status		SMALLINT NOT NULL DEFAULT 0 CHECK (status >= 0),
 						UNIQUE		(owner_id, secret)
 					)`,
 					`CREATE TABLE IF NOT EXISTS groups (
-						id			VARCHAR(254) PRIMARY KEY,
-						parent_id	VARCHAR(254),
-						owner_id	VARCHAR(254) NOT NULL,
+						id			VARCHAR(36) PRIMARY KEY,
+						parent_id	VARCHAR(36),
+						owner_id	VARCHAR(36) NOT NULL,
 						name		VARCHAR(1024) NOT NULL,
 						description	VARCHAR(1024),
 						metadata	JSONB,
 						created_at	TIMESTAMP,
 						updated_at	TIMESTAMP,
-						status		SMALLINT NOT NULL CHECK (status >= 0) DEFAULT 0,
+						status		SMALLINT NOT NULL DEFAULT 0 CHECK (status >= 0),
 						UNIQUE		(owner_id, name),
 						FOREIGN KEY	(parent_id) REFERENCES groups (id) ON DELETE CASCADE
 					)`,
 					`CREATE TABLE IF NOT EXISTS policies (
-						owner_id	VARCHAR(254) NOT NULL,
-						subject		VARCHAR(254) NOT NULL,
-						object		VARCHAR(254) NOT NULL,
+						owner_id	VARCHAR(36) NOT NULL,
+						subject		VARCHAR(36) NOT NULL,
+						object		VARCHAR(36) NOT NULL,
 						actions		TEXT[] NOT NULL,
 						created_at	TIMESTAMP,
 						updated_at	TIMESTAMP,
