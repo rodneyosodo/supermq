57a62926cb9a51d7605585daa3e7c9dea352582c
rodneyosodo <blackd0t@protonmail.com>
2023-07-28 11:58:37 +0300

    Remove Server Parse
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- internal/env/parser.go
+++ internal/env/parser.go
@@ -2,8 +2,6 @@ package env
 
 import (
 	"github.com/caarlos0/env/v7"
-	"github.com/mainflux/mainflux/internal/clients/grpc"
-	"github.com/mainflux/mainflux/internal/server"
 )
 
 type Options struct {
@@ -36,63 +34,5 @@ func Parse(v interface{}, opts ...Options) error {
 		})
 	}
 
-	switch cfg := v.(type) {
-	case *grpc.Config:
-		return parseGrpcConfig(cfg, altOpts...)
-	case *server.Config:
-		return parseServerConfig(cfg, altOpts...)
-	default:
-		return env.Parse(v, altOpts...)
-	}
-}
-
-func parseGrpcConfig(cfg *grpc.Config, opts ...env.Options) error {
-	if err := env.Parse(cfg, opts...); err != nil {
-		return err
-	}
-
-	if !cfg.ClientTLS || cfg.CACerts == "" {
-		altOpts := []env.Options{}
-		altOpts = append(altOpts, opts...)
-
-		altCfg := grpc.Config{}
-		if err := env.Parse(&altCfg, altOpts...); err != nil {
-			return err
-		}
-		if cfg.CACerts == "" && altCfg.CACerts != "" {
-			cfg.CACerts = altCfg.CACerts
-		}
-		if !cfg.ClientTLS && altCfg.ClientTLS {
-			cfg.ClientTLS = altCfg.ClientTLS
-		}
-	}
-
-	return nil
-}
-
-func parseServerConfig(cfg *server.Config, opts ...env.Options) error {
-	copyConfig := cfg
-	if err := env.Parse(cfg, opts...); err != nil {
-		return err
-	}
-
-	if cfg.CertFile == "" || cfg.KeyFile == "" || cfg.Port == "" || cfg.Port == copyConfig.Port {
-		altOpts := []env.Options{}
-		altOpts = append(altOpts, opts...)
-
-		altCfg := server.Config{}
-		if err := env.Parse(&altCfg, altOpts...); err != nil {
-			return err
-		}
-		if cfg.CertFile == "" && altCfg.CertFile != "" {
-			cfg.CertFile = altCfg.CertFile
-		}
-		if cfg.KeyFile == "" && altCfg.KeyFile != "" {
-			cfg.KeyFile = altCfg.KeyFile
-		}
-		if (cfg.Port == "" || cfg.Port == copyConfig.Port) && altCfg.Port != "" {
-			cfg.Port = altCfg.Port
-		}
-	}
-	return nil
+	return env.Parse(v, altOpts...)
 }
