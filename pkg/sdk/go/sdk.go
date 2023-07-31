7b115c8fec1d36cf42a6854e5f86312204d38ec1
rodneyosodo <blackd0t@protonmail.com>
2023-07-31 16:44:29 +0300

    Fix Issue After Rebasing
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- pkg/sdk/go/sdk.go
+++ pkg/sdk/go/sdk.go
@@ -699,8 +699,6 @@ type SDK interface {
 	//
 	// The subject in this case is the `userID` and the object is the `groupID`.
 	//
-	// The subject in this case is the `userID` and the object is the `groupID`.
-	//
 	// example:
 	//  aReq := sdk.AccessRequest{
 	//    Subject:    "userID:1",
