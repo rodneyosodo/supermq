6bcdd4f4bac06261838232e7f7594c9c2ccde3e4
rodneyosodo <blackd0t@protonmail.com>
2023-08-03 19:59:36 +0300

    Fix Share Thing To Add External To Request
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- pkg/sdk/go/policies.go
+++ pkg/sdk/go/policies.go
@@ -21,6 +21,7 @@ type Policy struct {
 	Subject   string    `json:"subject"`
 	Object    string    `json:"object"`
 	Actions   []string  `json:"actions"`
+	External  bool      `json:"external,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
