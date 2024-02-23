d7c6a91d07e1dcbb2dd09ffb55d44d6e4b5400bf
Musilah <nataleigh.nk@gmail.com>
2024-02-23 12:32:15 +0300

    fix CI
    Signed-off-by: Musilah <nataleigh.nk@gmail.com>

--- pkg/sdk/mocks/sdk.go
+++ pkg/sdk/mocks/sdk.go
@@ -1814,17 +1814,17 @@ func (_m *SDK) ReadMessages(pm sdk.MessagePageMeta, chanID string, token string)
 
 	var r0 sdk.MessagesPage
 	var r1 errors.SDKError
-	if rf, ok := ret.Get(0).(func(sdk.PageMetadata, string, string) (sdk.MessagesPage, errors.SDKError)); ok {
-		return rf(pm.PageMetadata, chanID, token)
+	if rf, ok := ret.Get(0).(func(sdk.MessagePageMeta, string, string) (sdk.MessagesPage, errors.SDKError)); ok {
+		return rf(pm, chanID, token)
 	}
-	if rf, ok := ret.Get(0).(func(sdk.PageMetadata, string, string) sdk.MessagesPage); ok {
-		r0 = rf(pm.PageMetadata, chanID, token)
+	if rf, ok := ret.Get(0).(func(sdk.MessagePageMeta, string, string) sdk.MessagesPage); ok {
+		r0 = rf(pm, chanID, token)
 	} else {
 		r0 = ret.Get(0).(sdk.MessagesPage)
 	}
 
-	if rf, ok := ret.Get(1).(func(sdk.PageMetadata, string, string) errors.SDKError); ok {
-		r1 = rf(pm.PageMetadata, chanID, token)
+	if rf, ok := ret.Get(1).(func(sdk.MessagePageMeta, string, string) errors.SDKError); ok {
+		r1 = rf(pm, chanID, token)
 	} else {
 		if ret.Get(1) != nil {
 			r1 = ret.Get(1).(errors.SDKError)
