d94f975e68668614806e6d3163b4d8563d281936
Rodney Osodo <socials@rodneyosodo.com>
2024-06-26 15:59:18 +0300

    docs(delete-user): Add docs for DeleteHandler cron job
    Add coumentation for the delete handler cron job based on what it does and why
    
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- users/delete_handler.go
+++ users/delete_handler.go
@@ -1,6 +1,12 @@
 // Copyright (c) Abstract Machines
 // SPDX-License-Identifier: Apache-2.0
 
+// The DeleteHandler is a cron job that runs periodically to delete users that have been marked as deleted
+// for a certain period of time together with the user's policies from the auth service.
+// The handler runs in a separate goroutine and checks for users that have been marked as deleted for a certain period of time.
+// If the user has been marked as deleted for more than the specified period,
+// the handler deletes the user's policies from the auth service and deletes the user from the database.
+
 package users
 
 import (
@@ -43,60 +49,52 @@ func NewDeleteHandler(ctx context.Context, clients postgres.Repository, auth mag
 			case <-ctx.Done():
 				return
 			case <-ticker.C:
-				handler.routine(ctx)
+				handler.handle(ctx)
 			}
 		}
 	}()
 }
 
-func (h *handler) routine(ctx context.Context) {
+func (h *handler) handle(ctx context.Context) {
 	pm := mgclients.Page{Limit: defLimit, Offset: 0, Status: mgclients.DeletedStatus}
 
-	dbUsers, err := h.clients.RetrieveAll(ctx, pm)
-	if err != nil {
-		h.logger.Error("failed to retrieve users", slog.Any("error", err))
-		return
-	}
+	for {
+		dbUsers, err := h.clients.RetrieveAll(ctx, pm)
+		if err != nil {
+			h.logger.Error("failed to retrieve users", slog.Any("error", err))
+			break
+		}
+		if dbUsers.Total == 0 {
+			break
+		}
 
-	if dbUsers.Total > defLimit {
-		for i := defLimit; i < dbUsers.Total; i += defLimit {
-			pm.Offset = i
-			du, err := h.clients.RetrieveAll(ctx, pm)
-			if err != nil {
-				h.logger.Error("failed to retrieve users", slog.Any("error", err), slog.Any("page", pm))
-				return
+		for _, u := range dbUsers.Clients {
+			if time.Since(u.UpdatedAt) < h.deleteAfter {
+				continue
 			}
 
-			dbUsers.Clients = append(dbUsers.Clients, du.Clients...)
-		}
-	}
+			deleteRes, err := h.auth.DeleteEntityPolicies(ctx, &magistrala.DeleteEntityPoliciesReq{
+				Id:         u.ID,
+				EntityType: auth.UserType,
+			})
+			if err != nil {
+				h.logger.Error("failed to delete user policies", slog.Any("error", err))
+				continue
+			}
+			if !deleteRes.Deleted {
+				h.logger.Error("failed to delete user policies", slog.Any("error", svcerr.ErrAuthorization))
+				continue
+			}
 
-	for _, u := range dbUsers.Clients {
-		if time.Since(u.UpdatedAt) < h.deleteAfter {
-			continue
-		}
+			if err := h.clients.Delete(ctx, u.ID); err != nil {
+				h.logger.Error("failed to delete user", slog.Any("error", err))
+				continue
+			}
 
-		deleteRes, err := h.auth.DeleteEntityPolicies(ctx, &magistrala.DeleteEntityPoliciesReq{
-			Id:         u.ID,
-			EntityType: auth.UserType,
-		})
-		if err != nil {
-			h.logger.Error("failed to delete user policies", slog.Any("error", err))
-			continue
+			h.logger.Info("user deleted", slog.Group("user",
+				slog.String("id", u.ID),
+				slog.String("name", u.Name),
+			))
 		}
-		if !deleteRes.Deleted {
-			h.logger.Error("failed to delete user policies", slog.Any("error", svcerr.ErrAuthorization))
-			continue
-		}
-
-		if err := h.clients.Delete(ctx, u.ID); err != nil {
-			h.logger.Error("failed to delete user", slog.Any("error", err))
-			continue
-		}
-
-		h.logger.Info("user deleted", slog.Group("user",
-			slog.String("id", u.ID),
-			slog.String("name", u.Name),
-		))
 	}
 }
